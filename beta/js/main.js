3c840abce06c9e4b3fa28d793b56de56ccf3f5e6
Nguyen Tri Thanh <thanhnt1@greenglobal.vn>
2016-04-26 10:00:09 +0700

    Update code

--- beta/js/main.js
+++ beta/js/main.js
@@ -1,9 +1,38 @@
-$.material.init();
-$('.select').dropdown({
-    autoinit: '.select'
+$(document).ready(function() {
+    // Material
+    $.material.init();
+    $('.select').dropdown({
+        autoinit: '.select'
+    });
+
+    // Date picker
+    var nowDate = new Date();
+    var today = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
+    $.fn.datepicker.defaults.language = 'vi';
+    $.fn.datepicker.defaults.format = 'dd/mm/yyyy';
+    $.fn.datepicker.defaults.autoclose = true;
+    $.fn.datepicker.defaults.weekStart = 1;
+    $.fn.datepicker.defaults.startDate = today;
+    $('.datepicker').datepicker();
+    $('.linked').change(function() {
+        var name = $(this).attr('name');
+        if (name) {
+            var type = $(this).attr('type');
+            var value = $(this).val();
+            var instance = null;
+            if (type == 'radio' || type == 'checkbox') {
+                var isChecked = $(this).is(':checked');
+                instance = $('.linked[name=' + name + '][value=' + value + ']');
+                instance.prop('checked', isChecked);
+            } else {
+                instance = $('.linked[name=' + name + ']');
+                instance.val(value);
+            }
+            if (value) {
+                instance.closest('.form-group').removeClass('is-empty');
+            } else {
+                instance.closest('.form-group').addClass('is-empty');
+            }
+        }
+    });
 });
-$.fn.datepicker.defaults.language = 'vi';
-$.fn.datepicker.defaults.format = 'dd/mm/yyyy';
-$.fn.datepicker.defaults.autoclose = true;
-$.fn.datepicker.defaults.weekStart = 1;
-$('.datepicker').datepicker();
